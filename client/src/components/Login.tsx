d9185d316349da1c24807566aa7b2386b4f33b28
Brian <briansterling93@yahoo.com>
2020-06-15 02:24:12 -0600

    login page

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { MainSection, SecondarySection } from "../styling/LoginStyling";
+
+const Login = () => {
+  return (
+    <MainSection>
+      <SecondarySection>Hello World</SecondarySection>
+    </MainSection>
+  );
+};
+
+export default Login;
---
+++ client/src/styling/LoginStyling.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const MainSection = styled.div`
+  height: 100vh;
+  background-color: #999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SecondarySection = styled.div`
+  display: flex;
+  justify-content: center;
+`;
