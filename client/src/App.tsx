8bb39beee31ba1838be508366ca5c478b5b81cd1
Rob Bertram <robert.bertram@livingasone.com>
2020-07-03 23:52:29 -0500

    fix context state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,8 +7,7 @@ import { faUserAlt, faKey } from "@fortawesome/free-solid-svg-icons";
 import Login from "./pages/Login";
 import NewOrg from "./pages/NewOrg";
 import Dashboard from "./pages/Dashboard";
-
-// import { StateContext, initialState, reducer } from "./context/StateContext";
+import { StateContext, initialState, reducer } from "./context/StateContext";
 
 const Universal = createGlobalStyle`
   body {
@@ -26,10 +25,10 @@ const Universal = createGlobalStyle`
 
 function App() {
   library.add(fab, faKey, faUserAlt);
-  // const [state, dispatch] = useReducer<any>(initialState, reducer);
+  const [state, dispatch] = useReducer<any>(initialState, reducer);
 
   return (
-    // <StateContext.Provider value={{ state, dispatch }}>
+    <StateContext.Provider value={{ state, dispatch }}>
     <Router>
       <Universal />
       <Switch>
@@ -38,7 +37,7 @@ function App() {
         <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </Router>
-    // </StateContext.Provider>
+    </StateContext.Provider>
   );
 }
 
--- client/src/context/StateContext.tsx
+++ client/src/context/StateContext.tsx
@@ -1,11 +1,12 @@
 import { createContext } from "react";
 
-interface IEmail {
+export interface IState {
   email: string;
-}
-
-interface IFirstName {
   first_name: string;
+  last_name: string;
+  organization_name: string;
+  url: string;
+  password: string;
 }
 
 export const initialState = {
@@ -17,22 +18,31 @@ export const initialState = {
   password: "",
 };
 
-export const appActions = {
-  UPDATE_EMAIL: (state: IEmail, actions: any) => {
+export enum APP_ACTIONS {
+  UPDATE_EMAIL = 'UPDATE_EMAIL',
+  UPDATE_FIRST_NAME = 'UPDATE_FIRST_NAME',
+  UPDATE_LAST_NAME = 'UPDATE_LAST_NAME'
+}
+
+type AppAction = { [key: string]: (state: IState, action: any) => IState }
+
+export const appActions: AppAction = {
+  [APP_ACTIONS.UPDATE_EMAIL]: (state: IState, actions: any) => {
     return { ...state, email: actions.payload };
   },
 
-  UPDATE_FIRST_NAME: (state: IFirstName, actions: any) => {
+  [APP_ACTIONS.UPDATE_FIRST_NAME]: (state: IState, actions: any) => {
     return { ...state, first_name: actions.payload };
   },
 
-  UPDATE_LAST_NAME: (state: any, actions: any) => {
+  [APP_ACTIONS.UPDATE_LAST_NAME]: (state: any, actions: any) => {
     return { ...state, last_name: actions.payload };
   },
 };
 
-// export const reducer = (state: any, action: any) => {
-//   return appActions[action.type](state, action);
-// };
+export const reducer = (state: IState, action: any) => {
+  return appActions[action.type](state, action);
+};
 
 export const StateContext: any = createContext(initialState);
+ 
